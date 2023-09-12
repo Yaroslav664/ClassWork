5d794a6b334e0628883adbe5f3033e6f1d037bfe
Yaroslav664 <133795990+Yaroslav664@users.noreply.github.com>
2023-09-12 20:40:02 +0300

    Create ConsoleApplication1.cpp

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void sayHi() {
+        cout << "Hello, my name is " << name;
+    }
+};
+
+Student olderStudent(Student students[]) {
+
+    Student oldest = students[0];
+
+    for (int i = 1; i < 3; i++) {
+        if (students[i].age > oldest.age) {
+            oldest = students[i];
+        }
+    }
+
+    return oldest;
+}
+
+int main() {
+Student newList[3]{ {"Vlad", 18},
+                    {"Maxim", 16},
+                    {"Oleg", 20} };
+
+Student oldestStudent = olderStudent(newList);
+
+
+ cout << "The oldest student is: " << oldestStudent.name << " (Age: " << oldestStudent.age << ")";
+}
