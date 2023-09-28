7db2cc6554a74c317ee17de4ad82636ae3d542de
Yaroslav664 <133795990+Yaroslav664@users.noreply.github.com>
2023-09-28 20:50:47 +0300

    Create ConsoleApplication5.cpp

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Bird {
+public:
+    string color;
+    float weight;
+    float height;
+
+    void Fly() {
+        cout << "The bird is flying." << endl;
+    }
+};
+
+class HomeParrot : public Bird {
+public:
+    string name;
+    bool isSpeak;
+    bool trained;
+
+    void Say(string word) {
+        cout << name << " says: " << word << endl;
+    }
+};
+
+class Eagle : public Bird {
+public:
+    bool isPredator;
+
+    void Hunt() {
+        if (isPredator) {
+            cout << "The eagle is hunting." << endl;
+        }
+        else {
+            cout << "This eagle is not a predator." << endl;
+        }
+    }
+};
+
+int main() {
+
+}
